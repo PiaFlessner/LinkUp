195106140944f7681199f99e00ea8134716dc534
mirco.albers <mirco.albers@stud.hs-emden-leer.de>
2022-10-12 21:17:22 +0200

    Methode angelegt

--- backup.py
+++ backup.py
@@ -1,3 +1,12 @@
+import os
+
+
+
+#Mirco: Methode um Metadaten der Datei zu erhalten
+def get_metadata(filepath:str):
+    
+    return 0
+
 if __name__ == "__main__":
     print("Hello World")
     

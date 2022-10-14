016ee43326022268d6d3c33b9c4a0047ea307fdc
mirco.albers <mirco.albers@stud.hs-emden-leer.de>
2022-10-14 18:06:01 +0200

    file Metadata first steps

---
+++ file.py
@@ -0,0 +1,14 @@
+from ctypes import sizeof
+from unicodedata import name
+
+from importlib_metadata import FileHash
+
+
+class File:
+    def __init__(self, name, f_hash, f_size, creation_date, modify):
+        self.name = name
+        self.f_hash = f_hash
+        self.f_size = f_size
+        self.creation_date = creation_date
+        self.modify = modify
+

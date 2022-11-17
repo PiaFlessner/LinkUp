105d05ae457f6f7c0a162187f23e9813d205540c
pia.flessner <pia.flessner@stud.hs-emden-leer.de>
2022-11-17 17:53:20 +0100

    start the work (get jewel)

--- datenbank.py
+++ datenbank.py
@@ -534,4 +534,32 @@ class Datenbank:
             for row in tmp:
                 answer.append(Jewel(row[0],self._decode_base64(row[1]),row[2],self._decode_base64(row[3]),self._decode_base64(row[4]),self._decode_base64(row[5])))
             return answer
+
+     def get_restore_Jewel(until_date:datetime, jewel_id:int):
+        conn = self.create_connection('datenbank.db')
+        answer= []
+
+        if conn != None:
+            cur = conn.cursor()
+            command = """SELECT Blob.ID_File, Max(Blob.Number) as Number, Jewel.FullbackupSource, Jewel.JewelSource, Blob.Source_Path, Blob.Origin_Name, Blob.Store_Destination FROM File
+                        INNER JOIN Blob on File.ID = Blob.ID_File
+                        INNER JOIN Jewel_File_Assignment on Jewel_File_Assignment.ID_File = File.ID
+                        INNER JOIN Jewel on Jewel.ID = Jewel_File_Assignment.ID_Jewel
+                        WHERE Jewel.ID = ?
+                        AND Blob.CreationDate >= ?
+                        GROUP BY Blob.ID_File;"""
+            params = (until_date, jewel_id)
+            cur.execute(command,params)
+            tmp = cur.fetchall()
+            for row in tmp:
+                pass
+            return answer
+
+
+
+
+     def get_restore_File(until_date:datetime):
+        pass
+
+     
             

804867c3dc4e94f51edda4e7e6fe5c740ab4ce01
miron.bodewes <miron.bodewes@stud.hs-emden-leer.de>
2022-11-19 22:32:34 +0100

    test_database anfang

--- test_database.py
+++ test_database.py
@@ -1,143 +1,61 @@
-import pathlib
-import time
+
 import unittest
 from backup import Backup
 import info_handler as ih
-import platform
-import os
 from filecmp import dircmp
 from filecmp import cmpfiles
 from datetime import datetime as date
 import datenbank
-import shutil
+from os.path import exists as path_exists
 
 device_name = "testCases"
 
 def are_dir_trees_equal(dir1, dir2):
     
-    dirs_cmp = dircmp(dir1, dir2) 
-    
-    if len(dirs_cmp.left_only) > 0 or len(dirs_cmp.right_only) > 0 or len(dirs_cmp.funny_files) > 0: # überprüfen, ob unterschiedliche Files oder Dirs vom Namen her
-                                                                                                     #existieren 
-        return False                                                                                 
-    
-    (match, mismatch, errors) =  cmpfiles(dir1, dir2, dirs_cmp.common_files, shallow=False)
-    
-    if len(mismatch) > 0 or len(errors) > 0:    # nach in halt überprüfen
-        return False
-    
-    for common_dir in dirs_cmp.common_dirs:
-        new_dir1 = os.path.join(dir1, common_dir)
-        new_dir2 = os.path.join(dir2, common_dir)
-        if not are_dir_trees_equal(new_dir1, new_dir2):
-            return False
-    return True
+    pass
 
 
-class TestRestore(unittest.TestCase):
+class TestDatabase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.daten = datenbank.Datenbank()
-        cls.config = ih.get_json_info(device_name)
-        cls.workingDirectory = str(pathlib.Path(__file__).parent.resolve())
+        pass
 
-        jewel_list = cls.config["jewel_sources"][device_name]
-        i = 0
-        for element in jewel_list:
-            jewel_list[i] = cls.workingDirectory + element
-            i= i+ 1
-
-        cls.backup = Backup(jewel_list, cls.workingDirectory + "/" + cls.config["destination"][device_name], True)
-        cls.backup.initialize_backup()
-        #time.sleep(1)
-
-    def test_a_restore_Jewel_only_Fullbackup(self):
-        restoreDay = date.today()
-        jewel = self.daten.get_restore_Jewel(restoreDay, 1)
-        self.assertTrue(jewel!= None,"An answer is None")
-        self.assertTrue(len(jewel[1]) == 1,"The lenght is incorrect")
-        self.assertTrue(jewel[0] == 1, f"The id is wrong. Must be 1, but is {jewel[0]}")
-        self.assertTrue(jewel[1][0][0] == 'test1.txt', 'Name is wrong')
-        self.assertTrue(jewel[1][0][1] == f'{self.workingDirectory}/unitTestFiles/jewel/test1.txt', f'jewel path is wrong. Its {jewel[1][0][1]}')
-        self.assertTrue(jewel[1][0][2] == f'{self.workingDirectory}/unitTestFiles/backupLocation/fullBackuptestCases/jewel/test1.txt', 'backup location is wrong')
-        self.assertTrue(jewel[1][0][3] == 1, f'Version Number is wrong. Should be 1, but is {jewel[1][0][3]}')
-
-    def test_b_restore_Jewel_date_in_past(self):
-        restoreDay = date.today()
-        restoreDay = restoreDay.replace(year=2000)
-        jewel = self.daten.get_restore_Jewel(restoreDay,1)
-        self.assertTrue(jewel== None,"The answer should be None")
-
-
-    def test_c_restore_File_only_Fullbackup(self):
-        restoreDay = date.today()
-        file = self.daten.get_restore_File(restoreDay, f"testCases{self.workingDirectory}/unitTestFiles/jewel/test1.txt")
-        self.assertTrue(file!= None,"An answer is None")
-
-        self.assertTrue(file[0] == 'test1.txt', 'Name is wrong')
-        self.assertTrue(file[1] == f'{self.workingDirectory}/unitTestFiles/jewel/test1.txt', f'jewel path is wrong: {file[1]}')
-        self.assertTrue(file[2] == f'{self.workingDirectory}/unitTestFiles/backupLocation/fullBackuptestCases/jewel/test1.txt', 'backup location is wrong')
-
-    def test_d_restore_jewel_diff_backup_change_file(self):
-      restoreDay = date.today()
-      file = open(f"{self.workingDirectory}/unitTestFiles/jewel/test1.txt", "a")
-      for i in range(5):
-          file.write("Change change change")
-      file.close()
-      #touch the the dir, for the real behavior of
-      os.utime(f"{self.workingDirectory}/unitTestFiles/jewel")
-
+    def test_a_init_database_exists(self):
+        daten = datenbank.Datenbank()
+        self.assertTrue(path_exists('datenbank.db'))
     
-      time.sleep(1)
-      self.backup.initialize_backup()
-      time.sleep(1)
-      jewel = self.daten.get_restore_Jewel(restoreDay,1)
-      self.assertTrue(jewel[1][0][3] == 2, f'Version Number is wrong. Should be 2, but is {jewel[1][0][3]}')
+    def test_b_init_same_tables_exist(self):
+        daten = datenbank.Datenbank()
+        conn = daten.create_connection('datenbank.db')
+        if conn != None:
+            cur = conn.cursor()
+            cur.execute("""SELECT name FROM sqlite_master WHERE type='table'""")
+            data = cur.fetchall()
+            for row in data:
+                print(row)
+
+
+    def test_c_init_all_columns_are_as_expected(self):
+        pass
+
+    def test_d_something(self):
+        pass
+
+    def addJewel(self,jewel):
+    def addJewelFileAssignment (self, id_jewel, id_file):
+    def check_which_jewel_sources_exist(self, jewel_source_arr, device_name):    
+    def get_Jewel_via_id(self,id):
+    def get_File_via_id(self,id):
+    def get_File_via_hash(self,hash):
+    def get_all_Files(self):
+    def get_Files_via_jewel_id(self,jewel_id):
 
    
-    def test_e_restore_Jewel_diff_backup_new_file(self):
-        restoreDay = date.today()
-        #create_new_file
-        file = open(os.path.join(os.path.dirname(__file__), "unitTestFiles/jewel/test_new.txt"), "a")
-
-        for i in range(5):
-            file.write("Hello World in test_new.txt\n")
-        file.close()
-        self.backup.initialize_backup()
-        time.sleep(1)
-        jewel = self.daten.get_restore_Jewel(restoreDay,1)
-        self.assertTrue(jewel!= None,"An answer is None")
-        self.assertTrue(len(jewel[1]) == 2,"The lenght is incorrect")
-
-
-    def test_f_restore_File_diff_backup(self):
-        restoreDay = date.today()
-        file = self.daten.get_restore_File(restoreDay, f"testCases{self.workingDirectory}/unitTestFiles/jewel/test1.txt")
-        self.assertTrue(file!= None,"An answer is None")
-        self.assertTrue(file[3] == 2, f"Version Number ist wrong, should be 2, is {file[3]}")
-
-
-    @classmethod
-    def tearDownClass(cls):
-
-        os.remove("datenbank.db")
-        shutil.rmtree(cls.config["destination"][device_name])
-        shutil.rmtree(cls.config["restore_destination"][device_name])
-        os.remove("unitTestFiles/jewel/test_new.txt")
 
 
 def suite():
   suite = unittest.TestSuite()
-
- 
-
-  #suite.addTest(TestRestore.test_a_restore_Jewel_only_Fullbackup)
-  #suite.addTest(TestRestore.test_b_restore_Jewel_date_in_past)
-  #suite.addTest(TestRestore.test_c_restore_File_only_Fullbackup)
-  #suite.addTest(TestRestore.test_d_restore_jewel_diff_backup_change_file)
-  suite.addTest(TestRestore.test_e_restore_Jewel_diff_backup_new_file)
-  #suite.addTest(TestRestore.test_f_restore_File_diff_backup)
   return suite
 
         

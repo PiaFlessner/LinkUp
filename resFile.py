68b877f07f299525fcab23324ca5cccedb73f8ee
pia.flessner <pia.flessner@stud.hs-emden-leer.de>
2022-12-02 22:48:28 +0100

    added two properties in resFile

--- resFile.py
+++ resFile.py
@@ -1,9 +1,11 @@
 class resFile:
-    def __init__(self, file_name: str, origin_location: str, backup_location: str, version_number: int):
+    def __init__(self, file_name: str, origin_location: str, backup_location: str, version_number: int, hash:str, reed_solomon_path:str=None):
         self._file_name = file_name
         self._origin_location = origin_location
         self._backup_location = backup_location
         self._version_number = version_number
+        self._hash = hash
+        self._reed_solomon_path = reed_solomon_path
 
     # getter and setter methods for class properties
     @property
@@ -37,3 +39,21 @@ class resFile:
     @version_number.setter
     def version_number(self, value):
         self._version_number = value
+        
+    @property
+    def hash(self):
+        return self._hash
+
+    @hash.setter
+    def hash(self, value):
+        self._hash = value
+
+    @property
+    def reed_solomon_path(self):
+        return self._reed_solomon_path
+
+    @reed_solomon_path.setter
+    def reed_solomon_path(self, value):
+        self._reed_solomon_path = value
+
+        

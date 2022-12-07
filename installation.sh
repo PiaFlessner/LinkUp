0a95ee070c2f9a36322921b25bb07441cff4d280
pia.flessner <pia.flessner@stud.hs-emden-leer.de>
2022-12-07 23:43:48 +0100

    corrected first idea

--- installation.sh
+++ installation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#python3 -m venv .venv_deploy
-#source .venv_deploy/bin/activate
-#.venv_deploy/bin/pip install -r requirements.txt
-sed -i "1i #!$(pwd)/.venv_deploy/bin/python3" $(pwd)/execute.py
+python3 -m venv .venv
+source .venv/bin/activate
+.venv/bin/pip install -r requirements.txt
+sed -i "1i #!$(pwd)/.venv/bin/python3" $(pwd)/execute.py
 chmod +x $(pwd)/execute.py
 ln -s $(pwd)/execute.py /usr/local/bin/backupper
